f2380d12df5f8d07bd3958e55717b536f1279213
Gunar Ortiz <gunarortiz@gmail.com>
2018-07-29 02:32:30 -0400

    first commit

---
+++ jonny.js
@@ -0,0 +1,70 @@
+
+const path = require('path');
+const express = require('express');
+const http = require('http');
+const SocketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = SocketIo.listen(server);
+
+// settings
+
+// routes
+app.get('/', (req, res) => {
+  res.sendFile(__dirname +'/index.html');
+});
+
+app.get('/user', (req, res) => {
+  res.sendFile(__dirname +'/user.html');
+});
+
+// static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+const SerialPort = require('serialport');
+const Readline = SerialPort.parsers.Readline;
+const parser = new Readline();
+
+const mySerial = new SerialPort('COM3', {
+  baudRate: 57600
+});
+
+mySerial.pipe(parser);
+
+mySerial.on('open', function () {
+  console.log('Opened Port.');
+});
+/*
+mySerial.on('data', function (data) {
+  console.log(data.toString());
+  io.emit('arduino:data', {
+    value: data.toString()
+  });
+});*/
+
+
+mySerial.on('data', function (data) {
+  console.log(data.toString());
+  io.emit('arduino:barra', {
+    value: data.toString()
+  });
+});
+
+
+mySerial.on('err', function (data) {
+  console.log(err.message);
+});
+
+
+server.listen(3000, () => {
+  console.log('Server on port 3000');
+});
+
+
+
+function sendMessage(a){
+  io.emit('arduino:message', {
+    value: a.toString()
+  });
+}
